a44578860d53deaecdcf2df83ddde19ee6f7199a
essmahao <samahaoum14@gmail.com>
2026-03-15 12:07:17 +0100

    finishing update a quiz

--- src/app/components/quiz-update-modal/quiz-update-modal.component.ts
+++ src/app/components/quiz-update-modal/quiz-update-modal.component.ts
@@ -27,12 +27,14 @@ import {
 } from '@ionic/angular/standalone';
 
 import { Quiz } from 'src/app/models/quiz';
+import { QuizService } from 'src/app/services/quiz';
+import { addIcons } from 'ionicons';
+import { removeOutline } from 'ionicons/icons'; 
 
 @Component({
   selector: 'app-quiz-update-modal',
   standalone: true,
-  template: `
-@if (isReady()) {
+  template: `@if (isReady()) {
   <form [formGroup]="quizForm" (ngSubmit)="confirm()" id="updateQuizForm" novalidate>
 
     <ion-header>
@@ -76,58 +78,66 @@ import { Quiz } from 'src/app/models/quiz';
       </ion-list>
 
       <ion-row formArrayName="questions">
-        @for (question of questionsArray.controls; track $index) {
-          <ion-col size="12" [formGroupName]="$index">
+
+        @for (question of questionsArray.controls; track $index; let qi = $index) {
+          <ion-col size="12" [formGroupName]="qi">
             <ion-card>
-              <ion-button
-                fill="clear"
-                color="medium"
-                class="ion-float-end"
-                (click)="removeQuestion($index)"
-              >
-                <ion-icon name="remove-outline"></ion-icon>
-              </ion-button>
+
               <ion-card-header>
-                <ion-card-title>
-                  <ion-input
-                    aria-label="Enter the question text"
-                    formControlName="text"
-                    placeholder="Question"
-                  ></ion-input>
-                </ion-card-title>
+                <ion-row style="align-items: center;">
+                  <ion-col>
+                    <ion-input
+                      aria-label="Enter the question text"
+                      formControlName="text"
+                      placeholder="Question"
+                    ></ion-input>
+                  </ion-col>
+                  <ion-col size="auto">
+                    <ion-button fill="clear" color="danger" (click)="removeQuestion(qi)">
+                      <ion-icon name="remove-outline"></ion-icon>
+                    </ion-button>
+                  </ion-col>
+                </ion-row>
               </ion-card-header>
+
               <ion-card-content>
                 <ion-radio-group formControlName="correctChoiceIndex">
                   <ion-list lines="none" formArrayName="choices">
+
                     <ion-item>
                       <ion-label>Choices</ion-label>
                       <ion-label slot="end">Correct</ion-label>
                     </ion-item>
-                    @for (choice of getChoices($index).controls; track $index; let first = $first; let idx = $index) {
-                      <ion-item [formGroupName]="idx">
+
+                    @for (choice of getChoices(qi).controls; track $index; let ci = $index; let first = $first) {
+                      <ion-item [formGroupName]="ci">
                         <ion-input
                           aria-label="Enter the choice text"
                           formControlName="text"
                           placeholder="Choice"
                         ></ion-input>
-                        <ion-radio slot="end" [value]="idx"></ion-radio>
-                        @if (!first) {
-                          <ion-button fill="clear" slot="end" color="medium" (click)="removeChoice($index, idx)">
+                        <ion-radio slot="end" [value]="ci"></ion-radio>
+                       
+                          <ion-button fill="clear" slot="end" color="danger" (click)="removeChoice(qi, ci)">
                             <ion-icon name="remove-outline"></ion-icon>
                           </ion-button>
-                        } @else {
+                      
                           <span slot="end" style="width: 2rem;"></span>
-                        }
+                        
                       </ion-item>
                     }
+
                   </ion-list>
                 </ion-radio-group>
-                <ion-button (click)="addChoice($index)" expand="full">Add choice</ion-button>
+                <ion-button (click)="addChoice(qi)" expand="full">Add choice</ion-button>
               </ion-card-content>
+
             </ion-card>
           </ion-col>
         }
+
       </ion-row>
+      <ion-button expand="block" (click)="addQuestion()">Add question</ion-button>
     </ion-content>
 
   </form>
@@ -162,6 +172,11 @@ export class QuizUpdateModalComponent implements OnInit {
 
   private readonly modalCtrl = inject(ModalController);
   private readonly fb = inject(FormBuilder);
+  private readonly q = inject(QuizService);
+
+    constructor() {
+    addIcons({ removeOutline });
+  }
 
   @Input() quiz!: Quiz;
 
@@ -176,9 +191,21 @@ export class QuizUpdateModalComponent implements OnInit {
   getChoices(questionIndex: number): FormArray {
     return this.questionsArray.at(questionIndex).get('choices') as FormArray;
   }
-
+  addQuestion() {
+    const newQuestionId = this.q.generateQuestionId(this.quiz.id);
+    this.questionsArray.push(
+      this.fb.group({       
+        id: [newQuestionId], 
+        text: ['', Validators.required],
+        correctChoiceIndex: [0],
+        choices: this.fb.array([  this.fb.group({ text: ['', Validators.required] })        ])
+      })
+    );
+  } 
+ 
   ngOnInit() {
     this.quizForm = this.fb.group({
+      id: [this.quiz.id],
       title: [this.quiz.title, Validators.required],
       description: [this.quiz.description],
       questions: this.fb.array(
